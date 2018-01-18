b3192852a9f4d27ee9981387439001b1d5ca8cfe
Victor <vcunha1503@gmail.com>
2018-01-17 22:31:13 -0200

    Initial commit

---
+++ client/app/app.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from'@angular/core'; 
+import { BrowserModule } from'@anfular/platform-browser';
+import { AppComponent } from './app.component';
+
+@NgModule({
+	imports: [BrowserModule],
+	declarations: [AppComponent],
+	bootstrap: [AppComponent]
+})
+
+class AppModule{}
